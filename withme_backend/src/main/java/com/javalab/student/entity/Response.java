a41911a73096d5a9e3d1ccfdba52c31c852868ea
Chungeuiyoung <atta01@hanmail.net>
2025-02-03 20:12:49 +0900

    response entity 최초 작성 - 오류가능있으니 머지는 추후에

---
+++ withme_backend/src/main/java/com/javalab/student/entity/Response.java
@@ -0,0 +1,36 @@
+package com.javalab.student.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+/**
+ * 응답 엔티티
+ * 설문에 대한 유저의 응답을 저장하는 테이블과 매핑
+ * 설문, 질문, 선택지 간의 관계를 저장
+ */
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "response")
+public class Response {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long responseId; // 응답 ID
+
+    @ManyToOne
+    @JoinColumn(name = "survey_id")
+    private Survey survey; // 해당 응답이 속한 설문
+
+    @ManyToOne
+    @JoinColumn(name = "question_id")
+    private Question question; // 해당 응답이 속한 질문
+
+    @ManyToOne
+    @JoinColumn(name = "choice_id")
+    private Choice choice; // 유저가 선택한 선택지
+}
