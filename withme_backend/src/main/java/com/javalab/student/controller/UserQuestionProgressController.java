e1f09ac27e7c450ebbe1c7aa198fa443e0b93c50
Chungeuiyoung <atta01@hanmail.net>
2025-02-04 16:28:58 +0900

    // 예외 처리 - 유효하지 않은 문진 진행 상태 요청     @ExceptionHandler(MethodArgumentNotValidException.class)     public ResponseEntity<String> handleValidationExceptions(MethodArgumentNotValidException ex) {         return ResponseEntity.badRequest().body("잘못된 요청: " + ex.getBindingResult().getAllErrors().get(0).getDefaultMessage());     } 예외 추가

--- withme_backend/src/main/java/com/javalab/student/controller/UserQuestionProgressController.java
+++ withme_backend/src/main/java/com/javalab/student/controller/UserQuestionProgressController.java
@@ -2,9 +2,14 @@ package com.javalab.student.controller;
 
 import com.javalab.student.entity.UserQuestionProgress;
 import com.javalab.student.service.UserQuestionProgressService;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /**
  * 유저 문진 진행 컨트롤러
@@ -26,18 +31,32 @@ public class UserQuestionProgressController {
      * 유저 문진 진행 상태 조회
      */
     @GetMapping("/{userId}")
-    public ResponseEntity<UserQuestionProgress> createUserQuestionProgress(@RequestBody UserQuestionProgress userQuestionProgress) {
+    public ResponseEntity<List<UserQuestionProgress>> getUserQuestionProgress(@PathVariable @NotNull String userId) {
+        return ResponseEntity.ok(userQuestionProgressService.getUserQuestionProgress(userId));
+    }
+
+    /**
+     * 새로운 유저 문진 진행 상태 생성
+     */
+    @PostMapping
+    public ResponseEntity<UserQuestionProgress> createUserQuestionProgress(@Valid @RequestBody UserQuestionProgress userQuestionProgress) {
         return ResponseEntity.ok(userQuestionProgressService.createUserQuestionProgress(userQuestionProgress));
     }
+
 
     /**
      * 유저 문진 진행 상태 삭제
      */
     @DeleteMapping("/{userId}/{questionnaireId}/{questionId}")
     public ResponseEntity<Void> deleteUserQuestionProgress(@PathVariable String userId, @PathVariable Long questionnaireId, @PathVariable Long questionId) {
-        userQuestionProgressService.deleteUserQuestionProgress(userId, questionnaireId,questionId);
+        userQuestionProgressService.deleteUserQuestionProgress(userId, questionnaireId, questionId);
         return ResponseEntity.noContent().build();
     }
 
+    // 예외 처리 - 유효하지 않은 문진 진행 상태 요청
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<String> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        return ResponseEntity.badRequest().body("잘못된 요청: " + ex.getBindingResult().getAllErrors().get(0).getDefaultMessage());
+    }
 
 }
