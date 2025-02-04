80271bacece372e1291d86ca558ba91b96d39bfe
Chungeuiyoung <atta01@hanmail.net>
2025-02-04 10:12:40 +0900

    UserSelectedTopicsDTO 최초 작성 - 오류가능있으니 머지는 추후에

---
+++ withme_backend/src/main/java/com/javalab/student/dto/UserSelectedTopicsDTO.java
@@ -0,0 +1,18 @@
+package com.javalab.student.dto;
+
+import lombok.*;
+
+/**
+ * 유저가 선택한 주제 DTO
+ * 유저가 선택한 주제에 대한 정보를 클라이언트와 주고 받을 때 사용하는 객체
+ */
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UserSelectedTopicsDTO {
+    private String userId; // 유저 ID
+    private Long topicId; // 선택한 주제 ID
+}
