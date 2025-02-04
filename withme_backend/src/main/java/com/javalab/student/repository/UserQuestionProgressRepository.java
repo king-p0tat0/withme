10129cd0340f76e1b95c723579181bdb7377a4af
Chungeuiyoung <atta01@hanmail.net>
2025-02-04 11:52:30 +0900

    // 유저 ID로 선택한 주제 목록을 조회하는 메소드     List<UserSelectedTopics> findAllByUserId(String userId);
        // 유저 ID와 주제 ID로 선택한 주제를 삭제하는 메소드
        void deleteByUserIdAndTopicId(String userId, Long topicId); 코드 추가

--- withme_backend/src/main/java/com/javalab/student/repository/UserQuestionProgressRepository.java
+++ withme_backend/src/main/java/com/javalab/student/repository/UserQuestionProgressRepository.java
@@ -4,6 +4,8 @@ import com.javalab.student.entity.UserQuestionProgress;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 /**
  * 유저 문진 진행 Repository
  * UserQuestionProgress 엔티티에 대한 CRUD 작업을 처리하는 리포지토리
@@ -11,4 +13,10 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserQuestionProgressRepository extends JpaRepository<UserQuestionProgress, Long> {
+    // 유저 ID를 기준으로 문진 진행 상태 목록을 조회하는 메소드
+    List<UserQuestionProgress> findAllByUserId(String userId);
+
+    // 유저 ID, 문진 ID, 질문 ID를 기준으로 문진 진행 상태 삭제하는 메소드
+    void deleteByUserIdAndQuestionnaireIdAndQuestionId(String userId, Long questionnaireId, Long questionId);
+
 }
