236cc6b72eed0ae9d2aa7c8b663c80112cfd5e9c
Chungeuiyoung <atta01@hanmail.net>
2025-02-04 12:38:28 +0900

    SurveyController 최초 커밋, 머지는 오류가능성으로인해 추후에

---
+++ withme_backend/src/main/java/com/javalab/student/controller/SurveyController.java
@@ -0,0 +1,62 @@
+package com.javalab.student.controller;
+
+import com.javalab.student.entity.Survey;
+import com.javalab.student.service.SurveyService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * 설문 컨트롤러
+ * 설문 관련 요청을 처리하는 REST API 컨트롤러
+ */
+
+@RestController
+@RequestMapping("/api/surveys")
+public class SurveyController {
+
+    private final SurveyService surveyService;
+
+    @Autowired
+    public SurveyController(SurveyService surveyService) {
+        this.surveyService = surveyService;
+    }
+
+    /**
+     * 모든 설문 조회
+     */
+    @GetMapping
+    public ResponseEntity<List<Survey>> getAllSurveys() {
+        return ResponseEntity.ok(surveyService.getAllSurveys());
+    }
+
+    /**
+     * 설문 ID로 설문 조회
+     */
+    @GetMapping
+    public ResponseEntity<Survey> getSurveyById(@PathVariable Long surveyId) {
+        Optional<Survey> survey = surveyService.getSurveyById(surveyId);
+        return survey.map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    /**
+     * 새로운 설문 생성
+     */
+    @PostMapping
+    public ResponseEntity<Survey> createSurvey(@RequestBody Survey survey) {
+        return ResponseEntity.ok(surveyService.createSurvey(survey));
+    }
+
+    /**
+     * 설문 삭제
+     */
+    @DeleteMapping("/{surveyId}")
+    public ResponseEntity<Void> deleteSurvey(@PathVariable Long surveyId) {
+        surveyService.deleteSurvey(surveyId);
+        return ResponseEntity.noContent().build();
+    }
+}
