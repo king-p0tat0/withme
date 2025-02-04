2c74780f2cdf23f62e383a3e3c341803f00520dd
Chungeuiyoung <atta01@hanmail.net>
2025-02-04 11:32:06 +0900

    Response Service 최초 작성 - 오류가능있으니 머지는 추후에

---
+++ withme_backend/src/main/java/com/javalab/student/service/ResponseService.java
@@ -0,0 +1,46 @@
+package com.javalab.student.service;
+
+import com.javalab.student.entity.Response;
+import com.javalab.student.repository.ResponseRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * 응답 서비스
+ * 설문에 대한 유저 응답에 대한 비즈니스 로직을 처리하는 서비스 클래스
+ */
+
+@Service
+public class ResponseService {
+
+    private final ResponseRepository responseRepository;
+
+    @Autowired
+    public ResponseService(ResponseRepository responseRepository){
+        this.responseRepository = responseRepository;
+    }
+
+    /**
+     * 모든 응답 조회
+     */
+    public List<Response> getAllresponses() {
+        return responseRepository.findAll();
+    }
+
+    /**
+     * 응답 ID 로 응답 조회
+     */
+    public Response createResponse(Response response){
+        return responseRepository.save(response);
+    }
+
+    /**
+     * 응답 삭제
+     */
+    public void deleteResponse(Long responseId) {
+        responseRepository.deleteById(responseId);
+    }
+
+}
