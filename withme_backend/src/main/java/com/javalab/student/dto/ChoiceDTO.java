d86e943e747701f197ce65a2079bbd12b2a79ed6
Chungeuiyoung <atta01@hanmail.net>
2025-02-04 10:07:56 +0900

    ChoiceDTO 최초 작성 - 오류가능있으니 머지는 추후에

---
+++ withme_backend/src/main/java/com/javalab/student/dto/ChoiceDTO.java
@@ -0,0 +1,21 @@
+package com.javalab.student.dto;
+
+import lombok.*;
+
+/**
+ * 선택지 DTO
+ * 질문에 대한 선택지 정보를 클라이언트와 주고 받을 때 사용하는 객체
+ */
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ChoiceDTO {
+    private Long choiceId; //  선택지 ID
+    private Long questionId; // 해당 선택지가 속한 질문 ID
+    private String choiceText; //  선택지 텍스트
+    private Integer seq; // 선택지 순서
+    private Integer score; //  선택지 점수
+}
