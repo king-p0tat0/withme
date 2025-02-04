b608669888e7aff514aa84256eafb99baee47637
Chungeuiyoung <atta01@hanmail.net>
2025-02-04 10:22:44 +0900

    QuestionnaireDTO 최초 작성 - 오류가능있으니 머지는 추후에

---
+++ withme_backend/src/main/java/com/javalab/student/dto/QuestionnaireDTO.java
@@ -0,0 +1,29 @@
+package com.javalab.student.dto;
+
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+/**
+ * 문진 DTO
+ * 유저가 진행중인 문진에 대한 정보를 클라이언트와 주고 받을 때 사용하는 객체
+ * 설문 ID, 유저 ID, 반려동물 ID, 문진 상태, 시작일 등을 포함
+ */
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class QuestionnaireDTO {
+    private Long questionnaireId;  // 문진 ID
+    private Long surveyId;  // 설문 ID
+    private String userId;  // 유저 ID
+    private Long petId;  // 반려동물 ID
+    private ResponseStatus responseStatus;  // 문진 상태 (PENDING, IN_PROGRESS, COMPLETED)
+    private LocalDateTime createdAt;  // 문진 시작일
+
+    public enum ResponseStatus {
+        PENDING, IN_PROGRESS, COMPLETED
+    }
+}
