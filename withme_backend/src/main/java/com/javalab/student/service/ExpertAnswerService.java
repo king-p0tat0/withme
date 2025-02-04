112468db29ab7047c1910a58d9cb00c9c4c599b8
Chungeuiyoung <atta01@hanmail.net>
2025-02-04 12:16:04 +0900

    ExpertAnswerService 최초 커밋, 머지는 오류가능성으로인해 추후에

---
+++ withme_backend/src/main/java/com/javalab/student/service/ExpertAnswerService.java
@@ -0,0 +1,56 @@
+package com.javalab.student.service;
+
+import com.javalab.student.entity.ExpertAnswer;
+import com.javalab.student.repository.ExpertAnswerRepository;
+import com.javalab.student.repository.ExpertQuestionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * 전문가 답변 서비스
+ * 전문가 답변에 대한 비즈니스 로직을 처리하는 서비스 클래스
+ */
+
+@Service
+public class ExpertAnswerService {
+
+    private final ExpertAnswerRepository expertAnswerRepository;
+
+    @Autowired
+    public ExpertAnswerService(ExpertAnswerRepository expertAnswerRepository) {
+        this.expertAnswerRepository = expertAnswerRepository;
+    }
+
+    /**
+     * 모든 전문가 답변 조회
+     */
+    public List<ExpertAnswer> getAllExpertAnswers() {
+        return expertAnswerRepository.findAll();
+    }
+
+    /**
+     * 전문가 답변 ID 로 답변 조회
+     */
+
+    public Optional<ExpertAnswer> getExpertAnswerById(Long answerId) {
+        return expertAnswerRepository.findById(answerId);
+    }
+
+    /**
+     * 새로운 전문가 답변 생성
+     */
+    public ExpertAnswer createExpertAnswer(ExpertAnswer expertAnswer){
+        return expertAnswerRepository.save(expertAnswer);
+    }
+
+    /**
+     * 전문가 답변 삭제
+     */
+    public void deleteExpertAnswer(Long answerId) {
+        expertAnswerRepository.deleteById(answerId);
+    }
+
+}
