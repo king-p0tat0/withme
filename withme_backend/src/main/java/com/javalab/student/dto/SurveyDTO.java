82bf6cdce2b11454d136368b73f19f91cccc48be
Chungeuiyoung <atta01@hanmail.net>
2025-02-04 16:23:37 +0900

    유효성 검사 추가

--- withme_backend/src/main/java/com/javalab/student/dto/SurveyDTO.java
+++ withme_backend/src/main/java/com/javalab/student/dto/SurveyDTO.java
@@ -1,6 +1,8 @@
 package com.javalab.student.dto;
 
 import com.javalab.student.entity.Survey;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 import java.time.LocalDateTime;
@@ -18,11 +20,20 @@ import java.time.LocalDateTime;
 @Builder
 
 public class SurveyDTO {
-    private Long surveyId; // 설문 ID
-    private String surveyTitle; // 설문 제목
-    private String description; // 설문 설명
-    private Survey.SurveyType type; // 설문 유형(FREE, PAID, BOTH)
-    private LocalDateTime createdAt; //  설문 생성일
+
+    private Long surveyId;  // 설문 ID
+
+    @NotNull(message = "설문 제목은 필수입니다.")  // 유효성 검사 추가
+    @Size(min = 3, max = 100, message = "설문 제목은 3자 이상, 100자 이하로 입력해주세요.")
+    private String surveyTitle;  // 설문 제목
+
+    @Size(max = 500, message = "설문 설명은 500자 이하로 입력해주세요.")
+    private String description;  // 설문 설명
+
+    @NotNull(message = "설문 유형은 필수입니다.")
+    private SurveyType type;  // 설문 유형 (FREE, PAID, BOTH)
+
+    private LocalDateTime createdAt;  // 설문 생성일
 
     public enum SurveyType {
         FREE, PAID, BOTH
