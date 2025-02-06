df397b99a23ebaa67ccf91691b7db7be57e4ca70
Chungeuiyoung <atta01@hanmail.net>
2025-02-06 11:09:22 +0900

    QuestionRepository 백엔드 테스트 코드수정중

--- withme_backend/src/main/java/com/javalab/student/repository/QuestionRepository.java
+++ withme_backend/src/main/java/com/javalab/student/repository/QuestionRepository.java
@@ -1,15 +1,21 @@
 package com.javalab.student.repository;
 
 import com.javalab.student.entity.Question;
-import org.springframework.data.convert.ReadingConverter;
+import com.javalab.student.entity.SurveyTopic;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 /**
  * 질문 Repository
  * Question 엔티티에 대한 CRUD 작업을 처리하는 리포지토리
  */
-
 @Repository
 public interface QuestionRepository extends JpaRepository<Question, Long> {
+
+    /**
+     * ✅ 특정 설문 주제에 속한 질문 조회
+     */
+    List<Question> findBySurveyTopic(SurveyTopic surveyTopic);
 }
