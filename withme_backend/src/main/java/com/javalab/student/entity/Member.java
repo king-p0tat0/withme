da9a6d180c4635d0cac62f0af9db69d67fba3b0e
king-p0tat0 <wjdtjdbs2089@gmail.com>
2025-02-13 14:42:38 +0900

    회원가입시 ROLE=USER 로 설정

--- withme_backend/src/main/java/com/javalab/student/entity/Member.java
+++ withme_backend/src/main/java/com/javalab/student/entity/Member.java
@@ -83,7 +83,7 @@ public class Member extends BaseEntity{
         member.setPhone(memberFormDto.getPhone());
         member.setPoints(0);
         member.setSocial(false); // 일반 회원가입이므로 소셜 로그인 여부는 false
-        member.setRole(memberFormDto.getRole());  // 회원가입 시 사용자의 권한 : USER  [수정]
+        member.setRole(memberFormDto.getRole() != null ? memberFormDto.getRole() : Role.USER);  // 회원가입 시 사용자의 권한 : USER  [수정]
         return member;
     }
 
