de04a18b541567395371f9088bd981ba67b67b06
gamja <wjdtjdbs2089@gmail.com>
2025-02-18 00:08:47 +0900

    메인 머지 완료, db: withme_test1로 수정

--- withme_frontend/src/App.jsx
+++ withme_frontend/src/App.jsx
@@ -67,6 +67,7 @@ import CartList from "./component/shop/Cart/CartList";
 import Order from "./component/shop/Order/OrderDetail"
 import PayResult from "./component/shop/Order/PayResult"
 
+
 function App() {
   // 리덕스 스토어의 상태를 가져오기 위해 useSelector 훅 사용, auth 슬라이스에서 user, isLoggedIn 상태를 가져옴
   // user: 사용자 정보 객체, isLoggedIn: 로그인 여부
